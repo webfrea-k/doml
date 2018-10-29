60f728f802baeb387e3fe84bb0b2fc6d6e8f91ed
Webfreak <info@webfreak.si>
2018-10-29 18:31:14 +0100

    Initial commit

---
+++ app/src/main/java/webfreak/si/daysofmylife2/FragmentMyData.kt
@@ -0,0 +1,16 @@
+package webfreak.si.daysofmylife2
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_main.view.*
+
+class FragmentMyData : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val rootView = inflater.inflate(R.layout.fragment_main, container, false)
+        rootView.section_label.text = "My data"
+        return rootView
+    }
+}
