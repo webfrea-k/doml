116fc91547b86c48b89659e2ccfbceae47777c69
Webfreak <info@webfreak.si>
2018-10-31 11:21:20 +0100

    Initial outlived fragment

--- app/src/main/java/webfreak/si/daysofmylife2/FragmentOutlived.kt
+++ app/src/main/java/webfreak/si/daysofmylife2/FragmentOutlived.kt
@@ -5,12 +5,23 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.android.synthetic.main.fragment_main.view.*
+import android.widget.ArrayAdapter
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import kotlinx.android.synthetic.main.fragment_outlived.view.*
 
 class FragmentOutlived : Fragment() {
+    lateinit var mAdView : AdView
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val rootView = inflater.inflate(R.layout.fragment_main, container, false)
-        rootView.section_label.text = "Outlived"
+        val rootView = inflater.inflate(R.layout.fragment_outlived, container, false)
+        val testList = arrayOf("hu", "tu", "su", "send", "pe", "t", "tu", "su", "send", "pe", "t", "tu", "su", "send", "pe", "t", "tu", "su", "send", "pe", "t", "tu", "su", "send", "pe", "t", "tu", "su", "send", "pe", "t")
+        val adapter = ArrayAdapter(activity, android.R.layout.simple_list_item_1, testList)
+        rootView.outlived_list.adapter = adapter
+
+        mAdView = rootView.adView
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
         return rootView
     }
 }
--- app/src/main/java/webfreak/si/daysofmylife2/MainActivity.kt
+++ app/src/main/java/webfreak/si/daysofmylife2/MainActivity.kt
@@ -14,6 +14,7 @@ import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.ads.MobileAds
 
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_main.view.*
@@ -49,6 +50,8 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+
+        MobileAds.initialize(this, "ca-app-pub-9883575696396484~4631354970")
 
         container.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
             override fun onPageScrollStateChanged(state: Int) {}
